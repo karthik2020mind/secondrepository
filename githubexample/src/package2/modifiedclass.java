25eeada32a24015fc5b402669c544a30e5996320
karthik2020mind <mkarthikraja111@gmail.com>
2021-05-24 12:22:49 +0530

    modified in ownbranch

--- githubexample/src/package2/modifiedclass.java
+++ githubexample/src/package2/modifiedclass.java
@@ -4,6 +4,9 @@ public class modifiedclass {
 
 	public static void main(String[] args) {
 		System.out.println("Class A printing");
+		
+		System.out.println("in branch ownbranch");
+		System.out.println("in ownbranch");
 
 	}
 
