8b3ad2a7159aec873cd0280706c88237eed12262
karthik2020mind <mkarthikraja111@gmail.com>
2021-05-24 12:34:15 +0530

    modified in second

--- githubexample/src/package2/modifiedclass.java
+++ githubexample/src/package2/modifiedclass.java
@@ -5,8 +5,9 @@ public class modifiedclass {
 	public static void main(String[] args) {
 		System.out.println("Class A printing");
 		
-		System.out.println("in branch ownbranch");
-		System.out.println("in ownbranch");
+		
+		
+		System.out.println("in second");
 
 	}
 
