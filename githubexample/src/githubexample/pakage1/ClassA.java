f7182b5f933fc263996c48fd6f45e62716788e95
karthik2020mind <mkarthikraja111@gmail.com>
2021-05-24 11:16:05 +0530

    project added to global rep

---
+++ githubexample/src/githubexample/pakage1/ClassA.java
@@ -0,0 +1,10 @@
+package githubexample.pakage1;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		System.out.println("Class A printing");
+
+	}
+
+}
---
+++ githubexample/src/package2/ClassB.java
@@ -0,0 +1,9 @@
+package package2;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		System.out.println("Class B printing");
+	}
+
+}
